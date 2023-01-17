9d8c721f6960440d6ef5aca63b7a1ca0dead4d99
soniangn <nguyen.sonia@gmail.com>
2023-01-17 02:23:23 -0800

    done task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    number = len(sys.argv) - 1
+
+    if number == 0:
+        print("0 arguments.")
+    elif number == 1:
+        print("{} argument:".format(number))
+    else:
+        print("{} arguments:".format(number))
+    
+    for i in range(1, number + 1):
+        print("{}: {}".format(i, sys.argv[i]))
