cec04c8d4ac6fa1c44e0905a6f23d6f704b2cfaf
soniangn <nguyen.sonia@gmail.com>
2023-01-17 03:17:08 -0800

    task 4

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import hidden_4
+    n = dir(hidden_4)
+    for i in range(0, n):
+        if i[0:2] != "__":
+            print("{}".format(i))
