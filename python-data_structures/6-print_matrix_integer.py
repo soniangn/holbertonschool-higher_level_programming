c8e191e17c751ff2a0d405d68e34aa7ede2a4d5c
soniangn <nguyen.sonia@gmail.com>
2023-01-24 03:47:49 -0800

    done task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in range(len(matrix)):
-        for number in range(len(matrix[row])):
-            if number < len(matrix[row]) - 1:
-                space = ' '
-            else:
-                space = '\n'
-            print("{:d}".format(matrix[row][number]), end=space)
+    if matrix:
+        for row in range(len(matrix)):
+            for number in range(len(matrix[row])):
+                if number < len(matrix[row]) - 1:
+                    space = ' '
+                else:
+                    space = '\n'
+                print("{:d}".format(matrix[row][number]), end=space)
