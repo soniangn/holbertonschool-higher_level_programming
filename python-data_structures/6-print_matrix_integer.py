18f04e640786b43ff7a4eebffbc471d82033097c
soniangn <nguyen.sonia@gmail.com>
2023-01-24 03:53:41 -0800

    done task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
-        print()
+        print("\n")
     else:
         for row in range(len(matrix)):
             for number in range(len(matrix[row])):
