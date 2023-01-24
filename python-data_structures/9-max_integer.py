6cc6b34f3203abd1864082acffe46ddb1dd626aa
soniangn <nguyen.sonia@gmail.com>
2023-01-24 06:06:33 -0800

    done task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = 0
+    if not my_list:
+        return None
+
+    max = -1
+    
     for number in my_list:
+        
         if number > max:
             max = number
         else:
