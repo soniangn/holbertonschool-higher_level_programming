ca42fabfa840f3b6a995735ac6840a3fa8260cf9
soniangn <nguyen.sonia@gmail.com>
2023-01-24 05:43:20 -0800

    done task 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if not sentence:
-        (len(sentence), None)
+        return_tup = (len(sentence), None)
     else:
         return_tup = (len(sentence), sentence[0])
     return return_tup
