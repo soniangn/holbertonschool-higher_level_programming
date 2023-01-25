c07c51cc0ae895a846c55a7721a98fefbd77fbfe
soniangn <nguyen.sonia@gmail.com>
2023-01-25 08:33:05 -0800

    rectified task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None:
+    if roman_string is None or not isinstance(roman_string, str):
         return 0
-    if not isinstance(roman_string, str):
-        return 0
-
-    number = list(roman_string)
-    res = 0
 
     roman_dict = {
         "I": 1,
@@ -21,22 +16,12 @@ def roman_to_int(roman_string):
         "M": 1000
     }
 
-    idx = 0
-
-    if len(number) > 1:
-        for i in number:
-            if i != number[len(number)-1]:
-                if number[idx] == 'I' and number[idx + 1] == 'V':
-                    number[idx:idx + 2] = [''.join(number[idx:idx + 2])]
-                elif number[idx] == 'I' and number[idx + 1] == 'X':
-                    number[idx:idx + 2] = [''.join(number[idx:idx + 2])]
-                elif number[idx] == 'X' and number[idx + 1] == 'C':
-                    number[idx:idx + 2] = [''.join(number[idx:idx + 2])]
-                else:
-                    idx += 1
-
-    for key, value in roman_dict.items():
-        for i in number:
-            if i == key:
-                res += value
+    res = 0
+    for i in range(len(roman_string)):
+        current = roman_dict[roman_string[i]]
+        if i + 1 < len(roman_string) and roman_dict[roman_string[i + 1]] > current:
+            res -= current
+        else:
+            res += current
+    
     return res
