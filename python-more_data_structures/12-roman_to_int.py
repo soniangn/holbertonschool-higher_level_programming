7ce1b16916602d58735973d5be52afe91829786b
soniangn <nguyen.sonia@gmail.com>
2023-01-25 08:22:03 -0800

    rectified 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string == None:
+    if roman_string is None:
         return 0
     if not isinstance(roman_string, str):
         return 0
@@ -9,30 +9,31 @@ def roman_to_int(roman_string):
     res = 0
 
     roman_dict = {
-        "I": 1, 
-        "IV": 4, 
-        "V": 5, 
-        "IX": 9, 
+        "I": 1,
+        "IV": 4,
+        "V": 5,
+        "IX": 9,
         "X": 10,
         "L": 50,
         "XC": 90,
-        "C": 100, 
-        "D": 500, 
-        "M": 1000 
+        "C": 100,
+        "D": 500,
+        "M": 1000
     }
-    
+
     idx = 0
 
     if len(number) > 1:
         for i in number:
-            if number[idx] == 'I' and number[idx + 1] == 'V':
-                number[idx:idx + 2] = [''.join(number[idx:idx + 2])]
-            elif number[idx] == 'I' and number[idx + 1] == 'X':
-                number[idx:idx + 2] = [''.join(number[idx:idx + 2])]
-            elif number[idx] == 'X' and number[idx + 1] == 'C':
-                number[idx:idx + 2] = [''.join(number[idx:idx + 2])]
-            else:
-                idx += 1
+            if i != number[len(number)-1]:
+                if number[idx] == 'I' and number[idx + 1] == 'V':
+                    number[idx:idx + 2] = [''.join(number[idx:idx + 2])]
+                elif number[idx] == 'I' and number[idx + 1] == 'X':
+                    number[idx:idx + 2] = [''.join(number[idx:idx + 2])]
+                elif number[idx] == 'X' and number[idx + 1] == 'C':
+                    number[idx:idx + 2] = [''.join(number[idx:idx + 2])]
+                else:
+                    idx += 1
 
     for key, value in roman_dict.items():
         for i in number:
