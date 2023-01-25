26945a3155dcc3c5f9e3d293e35a37e0d6bc03a6
soniangn <nguyen.sonia@gmail.com>
2023-01-25 06:59:24 -0800

    done task 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string == None:
+        return 0
+    if not isinstance(roman_string, str):
+        return 0
+
+    number = list(roman_string)
+    res = 0
+
+    roman_dict = {
+        "I": 1, 
+        "IV": 4, 
+        "V": 5, 
+        "IX": 9, 
+        "X": 10,
+        "L": 50,
+        "C": 100, 
+        "D": 500, 
+        "M": 1000 
+    }
+    
+    idx = 0
+    
+    for i in number:
+        if number[idx] == 'I' and number[idx + 1] == 'V':
+            number[idx:idx + 2] = [''.join(number[idx:idx + 2])]
+        elif number[idx] == 'I' and number[idx + 1] == 'X':
+            number[idx:idx + 2] = [''.join(number[idx:idx + 2])]
+
+    for key, value in roman_dict.items():
+        for i in number:
+            if i == key:
+                res += value
+    return res
