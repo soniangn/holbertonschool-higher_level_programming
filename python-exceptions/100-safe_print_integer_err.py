9e2b6c157eb0eec5d26c04054cacf780339990cb
soniangn <nguyen.sonia@gmail.com>
2023-01-26 04:09:13 -0800

    done task 100

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -4,6 +4,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as err:
+    except (ValueError, TypeError) as err:
         sys.stderr.write("Exception: {}".format(err))
         return False
