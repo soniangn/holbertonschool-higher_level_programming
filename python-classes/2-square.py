c437ff722e1307dd0893948d95028410d7298df3
soniangn <nguyen.sonia@gmail.com>
2023-01-30 09:30:04 -0800

    done task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''defines a square'''
+
+
+class Square:
+    '''defines a square with size'''
+    def __init__(self, size=0):
+        self._Square__size = size
+        
+        if isinstance(size, int) == False:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
