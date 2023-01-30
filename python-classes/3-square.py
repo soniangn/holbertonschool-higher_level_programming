ade6e6e163d6a0396c5ee126099e92c1f6a6bb6a
soniangn <nguyen.sonia@gmail.com>
2023-01-30 09:53:47 -0800

    done task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''defines a square'''
+
+
+class Square:
+    '''defines a square with size'''
+    def __init__(self, size=0):
+        self._Square__size = size
+
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self._Square__size ** 2
