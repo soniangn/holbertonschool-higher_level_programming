13ac0d046e79dfe877abf388df8f08128941268a
soniangn <nguyen.sonia@gmail.com>
2023-01-31 07:03:13 -0800

    done task 6

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -9,15 +9,12 @@ class TestMaxInteger(unittest.TestCase):
     def test_no_args(self):
         self.assertEqual(max_integer([]), None)
 
-class TestMaxInteger(unittest.TestCase):
     def test_max_end(self):
         self.assertEqual(max_integer([1, 2, 3]), 3)
 
-class TestMaxInteger(unittest.TestCase):
     def test_beginning_end(self):
         self.assertEqual(max_integer([5, 1, 3]), 5)
 
-class TestMaxInteger(unittest.TestCase):
     def test_middle_end(self):
         self.assertEqual(max_integer([1, 5, 3]), 5)
 
