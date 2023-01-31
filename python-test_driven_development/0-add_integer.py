7f7f9484fb46ee5991fdc819c6e3e854034481e3
soniangn <nguyen.sonia@gmail.com>
2023-01-31 02:30:17 -0800

    done task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+"""module that outputs the sum of two integer"""
+
+
 def add_integer(a, b=98):
     """function that adds two integers"""
     if type(a) is not int and type(a) is not float:
-       raise TypeError("a must be an integer")
+        raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
     if isinstance(a, float) is True:
         a = int(a)
     if isinstance(b, float) is True:
         b = int(b)
-    
+
     return a + b
