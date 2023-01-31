9abf29862585263df4bae5c3ec27627e75624492
soniangn <nguyen.sonia@gmail.com>
2023-01-31 05:22:51 -0800

    done task 4

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" module with function print_square(size) """
+
+
+def print_square(size):
+    """prints a square with the character #"""
+    if isinstance(size, int) is False:
+        raise TypeError("size must be an integer") 
+    if size < 0:
+        raise TypeError("size must be >= 0")
+    if isinstance(size, float) is True and size < 0:
+        raise TypeError("size must be an integer") 
+        
+    for i in range(size):
+        print("#" * size)
