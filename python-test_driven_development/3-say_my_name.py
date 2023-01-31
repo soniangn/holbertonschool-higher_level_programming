509ab12ab8e55e947594cbea59fce6b38084e7ab
soniangn <nguyen.sonia@gmail.com>
2023-01-31 05:15:18 -0800

    done task 3

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """module that prints My name is <first name> <last name>"""
 
+
 def say_my_name(first_name, last_name=""):
     """function that prints My name is <first name> <last name>"""
     if type(first_name) is not str:
