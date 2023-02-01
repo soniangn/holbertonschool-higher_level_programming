422155d14ff752d2f2cc6a145065691c623a5b14
soniangn <nguyen.sonia@gmail.com>
2023-02-01 08:08:47 -0800

    done task 8

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -75,6 +75,6 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        if rect_1.area() > rect_2.area():
+        if rect_1.area() >= rect_2.area():
             return True
         return False
