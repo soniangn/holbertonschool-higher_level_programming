3d22b0c282c45f046bdb2120fa4231f5de03cfef
soniangn <nguyen.sonia@gmail.com>
2023-02-01 08:26:34 -0800

    rectified isinstance and return value

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -75,10 +75,8 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        if rect_1 != rect_1.__repr__:
+        if isinstance(rect_1, Rectangle) is False:
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if rect_2 != rect_2.__repr__:
+        if isinstance(rect_2, Rectangle) is False:
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rect_2.area():
-            return True
-        return False
+        return rect_1 if rect_1.area() >= rect_2.area() else rect_2
