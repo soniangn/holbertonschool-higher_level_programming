7be3811d844a77dcedfc954135627169a294b6a2
soniangn <nguyen.sonia@gmail.com>
2023-02-01 07:15:16 -0800

    done task 6

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -6,6 +6,7 @@ class Rectangle:
     """defines a rectangle by its width and height"""
 
     number_of_instances = 0
+    
     def __init__(self, width=0, height=0):
         """initializes the rectangle"""
         type(self).number_of_instances += 1
