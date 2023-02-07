275d7f4b7bb5de12f803cfada9cd5d8b7e427ac8
soniangn <nguyen.sonia@gmail.com>
2023-02-07 09:08:51 -0800

    done task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """module for subclass Square"""
 
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
-    """subclass square that inherits from Rectangle""" 
+    """subclass square that inherits from Rectangle"""
     def __init__(self, size):
+        """initializes square instance"""
+        self.integer_validator("size", size)
         self.__size = size
         super().__init__(self.__size, self.__size)
-    
-    def area(self):
-        return self.__size * self.__size
-
