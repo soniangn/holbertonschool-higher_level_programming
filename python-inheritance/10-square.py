df22d2b7107bda0bf0666574317a616caf5fa99d
soniangn <nguyen.sonia@gmail.com>
2023-02-07 09:09:30 -0800

    done task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """module for subclass Square"""
-
-
 Rectangle = __import__('9-rectangle').Rectangle
+
 
 class Square(Rectangle):
     """subclass square that inherits from Rectangle"""
