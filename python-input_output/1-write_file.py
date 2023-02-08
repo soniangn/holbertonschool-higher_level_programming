50f40413d64e9978ad202cdebbd443c168e70a17
soniangn <nguyen.sonia@gmail.com>
2023-02-08 04:29:16 -0800

    done task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,6 +3,7 @@
 
 
 def write_file(filename="", text=""):
+    """writes a string to a text file"""
     with open(filename, "w", encoding='utf-8') as f:
         write_data = f.write(text)
         return write_data
