de527f04929d58d81bd538060bb0eb678e1747d3
soniangn <nguyen.sonia@gmail.com>
2023-02-08 05:06:42 -0800

    done task 5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -10,4 +10,4 @@ def save_to_json_file(my_obj, filename):
     with open(filename, "w", encoding="utf-8") as f:
         json_obj = json.dumps(my_obj)
         wr_object = f.write(json_obj)
-        return wr_object 
+        return wr_object
