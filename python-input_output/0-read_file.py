0b210b9039d5df98cee2881a4214eb7884a70781
soniangn <nguyen.sonia@gmail.com>
2023-02-08 04:22:07 -0800

    done task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,4 +5,4 @@ def read_file(filename=""):
     """reads a text file"""
     with open(filename, 'r', encoding="utf-8") as f:
         read_data = f.read()
-        print(read_data)
+        print(read_data, end="")
