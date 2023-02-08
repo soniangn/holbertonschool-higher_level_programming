c1a0a71ab6413d80398b59fe6cdb6d705c0d081a
soniangn <nguyen.sonia@gmail.com>
2023-02-08 08:45:28 -0800

    done task 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,9 +2,6 @@
 """module for class_to_json method"""
 
 
-import json
-
-
 def class_to_json(obj):
     """returns the dictionary description with simple data structure"""
     """(list, dictionary, string, integer and boolean) for """
