213a81d714288e7e32ae816452aeb7652f5427e5
soniangn <nguyen.sonia@gmail.com>
2023-02-08 04:44:17 -0800

    done task 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,6 +3,7 @@
 
 
 def append_write(filename="", text=""):
+    """appends a string at the end of a text file"""
     with open(filename, "a", encoding="utf-8") as f:
         write_file = f.write(text)
         return write_file
