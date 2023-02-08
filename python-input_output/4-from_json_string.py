9d3f729c45e995c64c4721e8ad6f71276a5541bd
soniangn <nguyen.sonia@gmail.com>
2023-02-08 05:00:30 -0800

    done task 4

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -6,7 +6,7 @@ import json
 
 
 def from_json_string(my_str):
-    """returns an object (Python data structure)""" 
+    """returns an object (Python data structure)"""
     """represented by a JSON string"""
     json_str = json.loads(my_str)
     return json_str
