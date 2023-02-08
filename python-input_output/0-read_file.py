b2bab528f37a18f6e1fd8121a1c0b0443d9b2fef
soniangn <nguyen.sonia@gmail.com>
2023-02-08 04:21:13 -0800

    done task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""module for read_file method"""
 
 def read_file(filename=""):
+    """reads a text file"""
     with open(filename, 'r', encoding="utf-8") as f:
         read_data = f.read()
         print(read_data)
