7258d4b99617c98e62d3ae2447b86830cb442421
soniangn <nguyen.sonia@gmail.com>
2023-02-10 05:08:34 -0800

    done task 12

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Module for pascal_triangle method """
+
+
+def pascal_triangle(n):
+    """returns a list of lists of integers """
+    """representing the Pascal s triangle of n"""
+    if n <= 0:
+        return []
+    
+    new_list = [[] for i in range(n)]
+
+    new_list[0].append(1)
+    if n > 1:
+            new_list[1] = [1, 1]
+
+    if n > 2:
+        for row in range(2, n):
+            new_list[row].append(1)
+            for i in range(row - 1):
+                new_list[row].append(new_list[row - 1][i] + new_list[row - 1][i + 1])
+            new_list[row].append(1)
+    return new_list
