c20088b914e0937e921d70086b61c8b1d1e0edea
soniangn <nguyen.sonia@gmail.com>
2023-02-13 06:50:45 -0800

    rectified initialization

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """module for Square class"""
-from models.rectangle import Rectangle
+from rectangle import Rectangle
 
 
 class Square(Rectangle):
     """ creates an instance of Square that inherits from Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
         """ initializes a square """
-        super().__init__(height=size, width=size, x=x, y=y, id=id)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x,
