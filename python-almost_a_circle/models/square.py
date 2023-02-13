8ecc7eb7b5fed166378f43b03c18107e6a1f8707
soniangn <nguyen.sonia@gmail.com>
2023-02-13 06:17:56 -0800

    done task 10

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -2,13 +2,13 @@
 """module for Square class"""
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     """ creates an instance of Square that inherits from Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
         """ initializes a square """
         super().__init__(size, size, x, y, id)
-        self.size = size
 
     def __str__(self):
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, 
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x,
                                                  self.y, self.size)
