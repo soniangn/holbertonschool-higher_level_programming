788dce79699d09485dc735f9c9804aa016861d83
soniangn <nguyen.sonia@gmail.com>
2023-02-13 10:13:12 -0800

    done task 14

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """module for Base class"""
-
+import json
 
 class Base:
     """manage id attribute in all the classes"""
@@ -12,4 +12,11 @@ class Base:
             self.id = id
         else:
             Base.__nb_objects += 1
-            self.id = self.__nb_objects
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
