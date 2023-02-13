943b957832b18bd34245cfeb2e5a0d97754c99be
soniangn <nguyen.sonia@gmail.com>
2023-02-13 14:50:50 -0800

    done task 17

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-"""module for Base class"""
+""" module for Base class """
 import json
 
 
 class Base:
-    """manage id attribute in all the classes"""
+    """ 
+    represents base class for all others models 
+    
+    __nb_objects: number of instantiated base
+
+    """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """initializes the id"""
+        """ initializes the id """
         if id is not None:
             self.id = id
         else:
@@ -17,7 +23,29 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """ returns the JSON string representation of list_dictionaries """
         if list_dictionaries is None:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = "{}.json".format(cls.__name__)
+
+        print(list_objs)
+
+        with open(filename, "w", encoding="utf-8") as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                json_obj = cls.to_json_string(list_objs)
+                f.write(json_obj)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ returns list of json string representation """
+        if json_string is None:
+            return("[]")
+        else:
+            return json.loads(json_string)
