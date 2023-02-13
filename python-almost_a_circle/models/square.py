694b3f8de0b891a051873d35d4d6ee85564f58ee
soniangn <nguyen.sonia@gmail.com>
2023-02-13 06:27:11 -0800

    change self.width

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -11,4 +11,4 @@ class Square(Rectangle):
 
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x,
-                                                 self.y, self.size)
+                                                 self.y, self.width)
