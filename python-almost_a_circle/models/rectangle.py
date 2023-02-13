a81c59042d3e20eef930fd3135e28bb34bc60033
soniangn <nguyen.sonia@gmail.com>
2023-02-13 01:53:01 -0800

    done task 1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """module for Rectangle class"""
-from base import Base
+from models.base import Base
 
 class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
