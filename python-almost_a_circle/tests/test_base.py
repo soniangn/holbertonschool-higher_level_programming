88a46a76506b1fb08c93cfff32a15b773e5bd507
soniangn <nguyen.sonia@gmail.com>
2023-02-14 10:05:13 -0800

    test test

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -6,17 +6,16 @@ from models.base import Base
 
 class TestBase(unittest.TestCase):
     def test_no_args(self):
-        self.assertEqual(Base(), 1)
+        self.assertEqual(Base().id, 1)
     
     def test_next(self):
-        self.assertEqual(Base(), 1)
+        self.assertEqual(Base().id, 2)
 
     def test_id(self):
-        self.assertEqual(Base(89), 89)
+        self.assertEqual(Base(89).id, 89)
 
-    def test_to_json(self):
-        self.assertEqual(Base.to_json_string(None), [])
-
+    def test_to_json_none(self):
+        self.assertEqual(Base.to_json_string(None), "[]")
 
 if __name__ == '__main__':
     unittest.main()
