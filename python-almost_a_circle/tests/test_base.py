c974f6f3288ae987a82c05de764ed1f5b8401794
soniangn <nguyen.sonia@gmail.com>
2023-02-14 10:16:06 -0800

    test test

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
+""" Unittest for Base
+"""
 import unittest
 from models.base import Base
 
 
 class TestBase(unittest.TestCase):
-    def test_base_assign_id_auto_0(self):
-        b1 = Base()
-        self.assertEqual(b1.id, 1)
+    def test_no_args(self):
+        obj = Base()
+        self.assertEqual(obj.id, 1)
+    
+    def test_next(self):
+        obj = Base()
+        self.assertEqual(obj.id, 2)
 
-    def test_base_assign_id_auto_1(self):
-        b2 = Base()
-        self.assertEqual(b2.id, 2)
+    def test_id(self):
+        obj = Base(89)
+        self.assertEqual(obj.id, 89)
 
-    def test_base_assign_id_auto_2(self):
-        b3 = Base(89)
-        self.assertEqual(b3.id, 89)
+    def test_to_json_none(self):
+        obj = Base.to_json_string(None)
+        self.assertEqual(obj, "[]")
+
+    def test_to_json_empty(self):
+        obj = Base.to_json_string([])
+        self.assertEqual(obj, "[]")
+
+    
 
 if __name__ == '__main__':
     unittest.main()
