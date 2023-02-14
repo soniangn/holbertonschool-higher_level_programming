e196e96d594ab8152124ad4a1213609525f2ff17
soniangn <nguyen.sonia@gmail.com>
2023-02-14 10:00:29 -0800

    test test

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 """ Unittest for Base
 """
+import unittest
 from models.base import Base
 
 class TestBase(unittest.TestCase):
-    def test_bla(self):
+    def test_no_args(self):
+        self.assertEqual(Base(), 1)
+    
+    def test_next(self):
         self.assertEqual(Base(), 1)
 
+    def test_id(self):
+        self.assertEqual(Base(89), 89)
+
+    def test_to_json(self):
+        self.assertEqual(Base.to_json_string(None), [])
 
 
 if __name__ == '__main__':
