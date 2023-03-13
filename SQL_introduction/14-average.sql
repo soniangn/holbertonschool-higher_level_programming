3bee387644282dea0afa2d394ba47bc076d3e082
soniangn <nguyen.sonia@gmail.com>
2023-03-13 04:40:12 -0700

    done task 14

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,2 +1,5 @@
 -- computes the score average of all records in the table second_table
-SELECT AVG(score) FROM second_table;
+ALTER TABLE second_table ADD COLUMN average INT;
+
+INSERT INTO second_table (average)
+VALUES (AVG(SCORE));
