9f7f5c700990b365fca9547baa66ac8c0a14e4ca
soniangn <nguyen.sonia@gmail.com>
2023-03-14 10:27:29 -0700

    done task 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" lists all states with a name starting with N (upper N) 
+""" lists all states with a name starting with N (upper N)
 from the database hbtn_0e_0_usa
 
 Arguments:
