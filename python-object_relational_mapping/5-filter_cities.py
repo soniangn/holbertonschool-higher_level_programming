79b48d052e9dadcf219852242e788f6bb453f00c
soniangn <nguyen.sonia@gmail.com>
2023-03-14 12:19:53 -0700

    fixed PEP8

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""  takes in the name of a state 
-as an argument and lists all cities of that state, 
+"""  takes in the name of a state
+as an argument and lists all cities of that state,
 using the database hbtn_0e_4_usa
 
 Arguments:
