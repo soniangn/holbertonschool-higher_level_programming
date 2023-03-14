bad892bd80d990d285303aa2cd07d01959f020b0
soniangn <nguyen.sonia@gmail.com>
2023-03-14 14:19:56 -0700

    done task 6

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from sqlalchemy import (create_engine)
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+engine = create_engine('mysql+mysqldb://root:root@localhost:3306')
+    
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key = True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+Base.metadata.create_all(engine)
