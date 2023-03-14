746441cd4f9cf0c572a8603b4a1b4eb5f3efc3ca
soniangn <nguyen.sonia@gmail.com>
2023-03-14 14:21:44 -0700

    done task 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,6 +2,8 @@
 from sqlalchemy import (create_engine)
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+""" module"""
+
 
 Base = declarative_base()
 
