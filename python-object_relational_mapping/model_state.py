676955daa3ed143953858142ba2dada20b625e7c
soniangn <nguyen.sonia@gmail.com>
2023-03-14 14:21:27 -0700

    done task 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -7,10 +7,10 @@ Base = declarative_base()
 
 engine = create_engine('mysql+mysqldb://root:root@localhost:3306')
     
+if __name__ == "__main__":
+    class State(Base):
+        __tablename__ = 'states'
+        id = Column(Integer, primary_key = True, autoincrement=True, nullable=False)
+        name = Column(String(128), nullable=False)
 
-class State(Base):
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key = True, autoincrement=True, nullable=False)
-    name = Column(String(128), nullable=False)
-
-Base.metadata.create_all(engine)
+    Base.metadata.create_all(engine)
