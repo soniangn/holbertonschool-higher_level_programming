a95b71252bd7b859e79d6ce6f4c04a4e182c4a7a
soniangn <nguyen.sonia@gmail.com>
2023-03-15 17:40:08 -0700

    done task 14

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -8,7 +8,7 @@ from model_state import Base
 class City(Base):
     """ class City """
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, autoincrement=True, \
+    id = Column(Integer, primary_key=True, autoincrement=True,
                 nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
