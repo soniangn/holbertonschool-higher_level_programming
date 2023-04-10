f5391c26e90f5fb58165ca27e04c1ca92393e92b
soniangn <nguyen.sonia@gmail.com>
2023-04-10 01:00:39 -0700

    implements second biggest

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,6 @@ const process = require('process');
 if (process.argv.length > 3) {
   const arg = process.argv.slice(2).sort((function(a, b){return a - b}));
   const length = arg.length;
-  console.log(arg);
   console.log(arg[length - 2]);
 } else {
   console.log('0');
