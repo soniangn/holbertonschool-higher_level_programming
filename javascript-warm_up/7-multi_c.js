bb0468ecfdf363bdf9dc816cd112f9d23b476e36
soniangn <nguyen.sonia@gmail.com>
2023-04-09 22:51:40 -0700

    fixed semistandard

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const process = require('process');
 
-if (isNaN(process.argv[2])){
-    console.log('Missing number of occurrences');
+if (isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
 } else {
-    for (let x = process.argv[2]; x > 0; x--) {
-      console.log('C is fun');
-    }
-} 
+  for (let x = process.argv[2]; x > 0; x--) {
+    console.log('C is fun');
+  }
+}
