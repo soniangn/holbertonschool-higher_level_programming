c997f7a7bfc18ffbafa869d5e7074030665bc4f2
soniangn <nguyen.sonia@gmail.com>
2023-04-10 01:03:54 -0700

    fixed semistandard

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 const process = require('process');
 
 if (process.argv.length > 3) {
-  const arg = process.argv.slice(2).sort((function(a, b){return a - b}));
+  const arg = process.argv.slice(2).sort(function (a, b) { return a - b; });
   const length = arg.length;
   console.log(arg[length - 2]);
 } else {
