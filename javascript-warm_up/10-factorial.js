98cbfcf071b1ea70ae03353988835cd63454aed6
soniangn <nguyen.sonia@gmail.com>
2023-04-09 23:51:16 -0700

    fixed semistandard

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 const process = require('process');
 
-function factorial(number) {
-    let n = parseInt(number);
+function factorial (number) {
+  const n = parseInt(number);
 
-    if (n === 0 || isNaN(n)) {
-      return 1;
-    }
-    
-    if (n > 0) {
-      return (n * factorial(n - 1));
-    }
+  if (n === 0 || isNaN(n)) {
+    return 1;
+  }
+
+  if (n > 0) {
+    return (n * factorial(n - 1));
+  }
 }
-
-console.log(factorial(process.argv[2]));
+const number = process.argv[2];
+console.log(factorial(number));
