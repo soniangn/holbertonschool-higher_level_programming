cea7208087462c33684c5d85d4038ad5a37c16d5
soniangn <nguyen.sonia@gmail.com>
2023-04-18 04:41:02 -0700

    adds, removes and clears LI elements from a list

--- javascript-web_jquery/101-script.js
+++ javascript-web_jquery/101-script.js
@@ -1,14 +1,13 @@
 $(document).ready(function () {
   $('DIV#add_item').click(function () {
     $('UL.my_list').append('<li>Item</li>');
-})});
+  });
 
-$(document).ready(function () {
   $('DIV#remove_item').click(function () {
     $('.my_list li').last().remove();
-})});
+  });
 
-$(document).ready(function () {
   $('DIV#clear_list').click(function () {
     $('.my_list li').remove();
-})});
+  });
+});
